79fec17bf3b82d3190969dedbffa375f7fdd77c1
Keiichi Lindley <DGZN@users.noreply.github.com>
2023-05-17 02:31:03 -0700

    Boilerplate TDD

---
+++ test/pdf417.test.js
@@ -0,0 +1,32 @@
+const { inspect } = require('util')
+
+let chai = require('chai')
+, expect = chai.expect
+, should = chai.should()
+, sinon = require('sinon')
+, chaiAsPromised = require("chai-as-promised")
+, debug = require("debug")("PDF417-BARCODE-GEN:tests:debug")
+
+chai.use( require('chai-integer') )
+
+let {getRndInteger} = require('../src/util.js')
+
+describe('PDF417.js', async () => {
+
+beforeEach(async () => {
+
+})
+
+describe('getRndInteger(', async () => {
+
+    it('Should return a random integer', async () => {
+      getRndInteger().should.be.an.integer()
+    })
+    
+})
+
+afterEach(async () => {
+
+})
+
+})
